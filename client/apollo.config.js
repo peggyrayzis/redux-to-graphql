edbce684f731aa93bccd85ac4e1779e9b6b9f81e
Peggy Rayzis <peggyrayzis@gmail.com>
2019-04-12 00:59:21 +0200

    Added VS Code config

--- client/apollo.config.js
+++ client/apollo.config.js
@@ -2,7 +2,7 @@ module.exports = {
   client: {
     service: {
       name: 'redux-to-graphql',
-      url: 'https://localhost:4000',
+      url: 'http://localhost:4000',
       // url: 'https://redux-to-graphql.glitch.me/',
     },
   },
