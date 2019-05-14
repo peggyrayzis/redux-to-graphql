2bea37986a5c91dcf297f4741e5a25a4b168c5ed
Peggy Rayzis <peggyrayzis@gmail.com>
2019-05-14 09:33:33 -0400

    Change to CodeSandbox

--- client/apollo.config.js
+++ client/apollo.config.js
@@ -2,8 +2,8 @@ module.exports = {
   client: {
     service: {
       name: 'redux-to-graphql',
-      url: 'http://localhost:4000',
-      // url: 'https://redux-to-graphql.glitch.me/',
+      // url: 'http://localhost:4000',
+      url: 'https://m3507x64l8.sse.codesandbox.io/',
       includes: ['src/**/*.{ts,tsx,js,jsx}'],
     },
   },
